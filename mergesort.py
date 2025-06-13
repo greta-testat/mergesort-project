ff926b00f1a6cd21e086931992af16838a09969f
Greta Guntermann <greta.guntermann@stud.uni-goettingen.de>
2025-06-13 13:37:04 +0200

    Improve plotting according to lecture standards

--- mergesort.py
+++ mergesort.py
@@ -42,10 +42,31 @@ def mergeSort(list_to_sort_by_merge):
 import matplotlib.pyplot as plt
 
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-x = range(len(my_list))
-plt.plot(x, my_list)
+#x = range(len(my_list))
+#plt.plot(x, my_list)
+#plt.show()
+#mergeSort(my_list)
+#x = range(len(my_list))
+#plt.plot(x, my_list)
+#plt.show()
+
+
+
+plt.figure(figsize=(8, 4))
+plt.plot(x, my_list, marker='o')
+plt.title("Unsorted List")
+plt.xlabel("Index")
+plt.ylabel("Value")
+plt.grid(True)
 plt.show()
+
 mergeSort(my_list)
-x = range(len(my_list))
-plt.plot(x, my_list)
+
+plt.figure(figsize=(8, 4))
+plt.plot(x, my_list, marker='o', color='green')
+plt.title("Sorted List")
+plt.xlabel("Index")
+plt.ylabel("Value")
+plt.grid(True)
 plt.show()
+
